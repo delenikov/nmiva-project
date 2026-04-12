cab37b8be0b98a606386e39b1f114b81d4cf4939
delenikov <delenikov.stojan@gmail.com>
2026-04-12 18:53:04 +0200

    first commit

---
+++ src/main/java/com/delenikov/nmiva/NmivaApplication.java
@@ -0,0 +1,13 @@
+package com.delenikov.nmiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmivaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NmivaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/delenikov/nmiva/NmivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delenikov.nmiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmivaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
